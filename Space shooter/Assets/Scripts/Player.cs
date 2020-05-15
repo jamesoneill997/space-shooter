d7f21d0d649192359c56ba979715d55c2027004b
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-15 10:22:47 +0100

    added wrapping and movement

--- Space shooter/Assets/Scripts/Player.cs
+++ Space shooter/Assets/Scripts/Player.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 //Player Extends MonoBehaviour
 public class Player : MonoBehaviour
-{
+{   
+    //speed setting
+    [SerializeField]
+    private int _speed = 5;
+    public float horizontalInput;
+    public float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        //curr position = 0
         transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {  
-        //z axis 1 unit per second
-        transform.Translate(Vector3.forward, Time.deltaTime);
+        //user input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalBound = 5.0f;
+        float verticalBound = 9.23f;
 
-        //
+        Vector3 direction = new Vector3(horizontalInput,verticalInput,0);
+        transform.Translate(direction*_speed*Time.deltaTime);
         
+
+        if(transform.position.y >= horizontalBound || transform.position.y <= -1*horizontalBound){
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x >= verticalBound || transform.position.x <= -1*verticalBound){
+            transform.position = new Vector3(-1*transform.position.x, transform.position.y, transform.position.z);
+        }
     }
 }
