58dc376b4649cb02ca32fe0d3cc3c34b81867705
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-15 08:06:46 +0100

    init commit

---
+++ Space shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Player Extends MonoBehaviour
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //curr position = 0
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        //z axis 1 unit per second
+        transform.Translate(Vector3.forward, Time.deltaTime);
+
+        //
+        
+    }
+}
